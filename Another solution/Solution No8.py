b6668142bd1874825541c0d6c2a5b0e74a9500c5
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-16 21:56:29 +0600

    Add files via upload

---
+++ Another solution/Solution No8.py
@@ -0,0 +1,18 @@
+import collections
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        c = collections.Counter(nums1)
+        ans = []
+        for x in nums2:
+            if c[x] > 0:
+                ans += x,
+                c[x] -= 1
+        return ans
+
+adding = Solution()
+nums1 = list(map(int,input('list1: ').split()))
+nums2 = list(map(int,input('list2: ').split()))
+x= adding.intersect(nums1, nums2)
+print('The intersection of two sets:', x)
